8bac1c81c0b68fa57b2c17629dbc8baa0d10abe0
vincentdang92 <vincentdang92@gmail.com>
2021-07-01 12:19:28 +0700

    init SSL

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if ( process.env.NODE_ENV === 'production' ) {
     cert: certificate,
     ca: ca
   }
-  server = https.createServer(httpsOptions, app).listen(443, '103.124.95.81')
+  server = https.createServer(httpsOptions, app).listen(443, '103.159.51.130')
   server1 = http.createServer(app).listen(80, '103.159.51.130')
 }
 // We instantiate nuxt.js with the options
